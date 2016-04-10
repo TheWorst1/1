c3edcfb8da745b89ad843cb4cbe40d4eb807872b
TheWorst1 <woesdo@chmail.ir>
2016-04-10 15:15:02 +0430

    Update setsticker.lua

--- plugins/setsticker.lua
+++ plugins/setsticker.lua
@@ -6,8 +6,8 @@ local function tosticker(msg, success, result)
     os.rename(result, file)
     print('File moved to:', file)
     send_document(get_receiver(msg), file, ok_cb, false)
-	send_large_msg(receiver, 'درست شدددد', ok_cb, false)
-    redis:del("photo:تبدیل")
+	send_large_msg(receiver, 'Done!', ok_cb, false)
+    redis:del("photo:setsticker")
   else
     print('Error downloading: '..msg.id)
     send_large_msg(receiver, 'Failed, please try again!', ok_cb, false)
@@ -17,25 +17,25 @@ local function run(msg,matches)
     local receiver = get_receiver(msg)
     local group = msg.to.id
     if msg.media then
-       if msg.media.type == 'به استیکر' and redis:get("photo:تبدیل") then
-        if redis:get("photo:تبدیل") == 'waiting' then
+       if msg.media.type == 'رفیق' and redis:get("photo:setsticker") then
+        if redis:get("photo:setsticker") == 'waiting' then
           load_photo(msg.id, tosticker, msg)
         end
        end
     end
-    if matches[1]:lower() == "تبدیل" and is_sudo(msg) then
-     redis:set("photo:تبدیل", "waiting")
-     return 'عکسو بده تبدیل کنم :)'
+    if matches[1]:lower() == "setsticker" and is_sudo(msg) then
+     redis:set("photo:setsticker", "waiting")
+     return 'Please send your photo now'
     end
-	if matches[1]:lower() == 'تبدیل به استیکر' then --[[Your bot name]]
+	if matches[1]:lower() == 'ذخانیات' then --[[Your bot name]]
 	send_document(get_receiver(msg), "sticker.webp", ok_cb, false)
 end
 end
 return {
   patterns = {
- "^به استیکر$",
- "^تبدیل$",
- "^تبدیل به استیکر",
+ "^(setsticker)$",
+ "^(دخانیات)$",
+ "^رفیق$",
  "%[(photo)%]",
   },
   run = run,
