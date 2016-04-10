b209e6036664026eeab9e9e7092c01f0ac4b0924
TheWorst1 <woesdo@chmail.ir>
2016-04-10 15:05:02 +0430

    Update setsticker.lua

--- plugins/setsticker.lua
+++ plugins/setsticker.lua
@@ -7,7 +7,7 @@ local function tosticker(msg, success, result)
     print('File moved to:', file)
     send_document(get_receiver(msg), file, ok_cb, false)
 	send_large_msg(receiver, 'درست شدددد', ok_cb, false)
-    redis:del("photo:setsticker")
+    redis:del("photo:تبدیل")
   else
     print('Error downloading: '..msg.id)
     send_large_msg(receiver, 'Failed, please try again!', ok_cb, false)
@@ -17,14 +17,14 @@ local function run(msg,matches)
     local receiver = get_receiver(msg)
     local group = msg.to.id
     if msg.media then
-       if msg.media.type == 'به استیکر' and redis:get("photo:setsticker") then
-        if redis:get("photo:setsticker") == 'waiting' then
+       if msg.media.type == 'به استیکر' and redis:get("photo:تبدیل") then
+        if redis:get("photo:تبدیل") == 'waiting' then
           load_photo(msg.id, tosticker, msg)
         end
        end
     end
     if matches[1]:lower() == "تبدیل" and is_sudo(msg) then
-     redis:set("photo:setsticker", "waiting")
+     redis:set("photo:تبدیل", "waiting")
      return 'عکسو بده تبدیل کنم :)'
     end
 	if matches[1]:lower() == 'تبدیل به استیکر' then --[[Your bot name]]
